d5bd7bec2c4ab328beb4269c6cba601007f3991b
Olivier Crête <olivier.crete@collabora.co.uk>
2008-09-04 19:30:20 -0400

    Add bare threading container

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ LDFLAGS =  `pkg-config --libs gupnp-1.0`
 
 fs-upnp-simple-igd.o: fs-upnp-simple-igd-marshal.h fs-upnp-simple-igd.c fs-upnp-simple-igd.h
 
-test: fs-upnp-simple-igd-marshal.o fs-upnp-simple-igd.o test.o 
+fs-upnp-simple-igd-thread.o: fs-upnp-simple-igd-thread.c fs-upnp-simple-igd-thread.h
+
+test: fs-upnp-simple-igd-marshal.o fs-upnp-simple-igd.o test.o fs-upnp-simple-igd-thread.o
 
 clean:
 	rm -f *.o test
---
+++ fs-upnp-simple-igd-thread.c
@@ -0,0 +1,77 @@
+/*
+ * Farsight2 - Farsight UPnP IGD abstraction
+ *
+ * Copyright 2008 Collabora Ltd.
+ *  @author: Olivier Crete <olivier.crete@collabora.co.uk>
+ * Copyright 2008 Nokia Corp.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+
+#include "fs-upnp-simple-igd-thread.h"
+
+
+struct _FsUpnpSimpleIgdThreadPrivate
+{
+  GThread *thread;
+  GMainLoop *loop;
+  GMainContext *context;
+};
+
+
+
+
+#define FS_UPNP_SIMPLE_IGD_THREAD_GET_PRIVATE(o)                        \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), FS_TYPE_UPNP_SIMPLE_IGD_THREAD,    \
+   FsUpnpSimpleIgdThreadPrivate))
+
+
+G_DEFINE_TYPE (FsUpnpSimpleIgdThread, fs_upnp_simple_igd_thread,
+    FS_TYPE_UPNP_SIMPLE_IGD);
+
+static void fs_upnp_simple_igd_thread_constructed (GObject *object);
+static void fs_upnp_simple_igd_thread_finalize (GObject *object);
+
+
+static void
+fs_upnp_simple_igd_thread_class_init (FsUpnpSimpleIgdThreadClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (FsUpnpSimpleIgdThreadPrivate));
+
+  gobject_class->constructed = fs_upnp_simple_igd_thread_constructed;
+  gobject_class->finalize = fs_upnp_simple_igd_thread_finalize;
+}
+
+
+static void
+fs_upnp_simple_igd_thread_init (FsUpnpSimpleIgdThread *self)
+{
+  self->priv = FS_UPNP_SIMPLE_IGD_THREAD_GET_PRIVATE (self);
+}
+
+static void
+fs_upnp_simple_igd_thread_finalize (GObject *object)
+{
+  //FsUpnpSimpleIgdThread *self = FS_UPNP_SIMPLE_IGD_THREAD_CAST (object);
+}
+
+static void
+fs_upnp_simple_igd_thread_constructed (GObject *object)
+{
+  //FsUpnpSimpleIgdThread *self = FS_UPNP_SIMPLE_IGD_THREAD_CAST (object);
+}
