aee71c79d115ee110027b21dacd2579d8812b926
Olivier Crête <olivier.crete@collabora.co.uk>
2008-09-04 16:06:26 -0400

    Add test for port mapping

--- test.c
+++ test.c
@@ -1,16 +1,34 @@
 
+#include <stdlib.h>
+
 #include <glib.h>
 
 #include "fs-upnp-simple-igd.h"
 
 GMainLoop *loop = NULL;
+  FsUpnpSimpleIgd *igd = NULL;
+  guint external_port, internal_port;
+
+static gboolean
+_remove_port (gpointer user_data)
+{
+  g_debug ("removing port");
+  fs_upnp_simple_igd_remove_port (igd, "TCP", external_port);
+
+  return FALSE;
+}
 
 static void
-_new_external_ip (FsUpnpSimpleIgd *igd, gchar *ip, gpointer user_data)
+_mapped_external_port (FsUpnpSimpleIgd *igd, gchar *proto,
+    gchar *external_ip, gchar *replaces_external_ip, guint external_port,
+    gchar *local_ip, guint local_port,
+    gchar *description, gpointer user_data)
 {
-  g_debug ("ip: %s", ip);
+  g_debug ("proto:%s ex:%s oldex:%s exp:%u local:%s localp:%u desc:%s",
+      proto, external_ip, replaces_external_ip, external_port, local_ip,
+      local_port, description);
 
-  g_main_loop_quit (loop);
+  g_timeout_add (3000, _remove_port, user_data);
 }
 
 
@@ -22,7 +40,17 @@ _error (FsUpnpSimpleIgd *igd, GError *error, gpointer user_data)
 int
 main (int argc, char **argv)
 {
-  FsUpnpSimpleIgd *igd = NULL;
+
+  if (argc != 5)
+  {
+    g_print ("Usage: %s <external port> <local ip> <local port> <description>\n",
+        argv[0]);
+    return 0;
+  }
+
+  external_port = atoi (argv[1]);
+  internal_port = atoi (argv[3]);
+  g_return_val_if_fail (external_port && internal_port, 1);
 
   g_type_init ();
   g_thread_init (NULL);
@@ -31,10 +59,14 @@ main (int argc, char **argv)
 
   igd = fs_upnp_simple_igd_new (NULL);
 
-  g_signal_connect (igd, "new-external-ip", G_CALLBACK (_new_external_ip),
+  g_signal_connect (igd, "mapped-external-port",
+      G_CALLBACK (_mapped_external_port),
       NULL);
   g_signal_connect (igd, "error", G_CALLBACK (_error),
       NULL);
+
+  fs_upnp_simple_igd_add_port (igd, "TCP", external_port, argv[2],
+      internal_port, 600, argv[4]);
 
   g_main_loop_run (loop);
 
