f169dec1535dbc00699fa801b50309fe0198a2c1
Olivier Crête <olivier.crete@collabora.co.uk>
2009-07-29 16:55:37 -0400

    rtpsession: Defer dispose to another thread if dispose is called from a streaming thread

--- gst/fsrtpconference/fs-rtp-session.c
+++ gst/fsrtpconference/fs-rtp-session.c
@@ -471,22 +471,19 @@ stop_and_remove (GstBin *conf, GstElement **element, gboolean unref)
   *element = NULL;
 }
 
-static void
-fs_rtp_session_dispose (GObject *object)
+
+static gpointer
+trigger_dispose (gpointer data)
 {
-  FsRtpSession *self = FS_RTP_SESSION (object);
+  g_object_unref (data);
+  return NULL;
+}
+
+static void
+fs_rtp_session_real_dispose (FsRtpSession *self)
+{
   GList *item = NULL;
   GstBin *conferencebin = NULL;
-
-  g_static_rw_lock_writer_lock (&self->priv->disposed_lock);
-  if (self->priv->disposed)
-  {
-    /* If dispose did already run, return. */
-    g_static_rw_lock_writer_unlock (&self->priv->disposed_lock);
-    return;
-  }
-  g_static_rw_lock_writer_unlock (&self->priv->disposed_lock);
-
 
   conferencebin = GST_BIN (self->priv->conference);
 
@@ -694,10 +691,36 @@ fs_rtp_session_dispose (GObject *object)
   g_hash_table_remove_all (self->priv->ssrc_streams);
   g_hash_table_remove_all (self->priv->ssrc_streams_manual);
 
-  /* MAKE sure dispose does not run twice. */
+  G_OBJECT_CLASS (fs_rtp_session_parent_class)->dispose (G_OBJECT (self));
+}
+
+static void
+fs_rtp_session_dispose (GObject *object)
+{
+  FsRtpSession *self = FS_RTP_SESSION (object);
+
+  g_static_rw_lock_writer_lock (&self->priv->disposed_lock);
+  if (self->priv->disposed)
+  {
+    /* If dispose did already run, return. */
+    g_static_rw_lock_writer_unlock (&self->priv->disposed_lock);
+    return;
+  }
+
   self->priv->disposed = TRUE;
 
-  G_OBJECT_CLASS (fs_rtp_session_parent_class)->dispose (object);
+  g_static_rw_lock_writer_unlock (&self->priv->disposed_lock);
+
+  if (fs_rtp_conference_is_internal_thread (self->priv->conference))
+  {
+    g_object_ref (self);
+    if (!g_thread_create (trigger_dispose, self, FALSE, NULL))
+      g_error ("Could not create dispose thread");
+  }
+  else
+  {
+    fs_rtp_session_real_dispose (self);
+  }
 }
 
 static void
