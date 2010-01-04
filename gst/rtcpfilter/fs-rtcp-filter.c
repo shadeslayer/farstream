327e825d42c46678e6d4ba818af98c8c9e9633b0
Olivier Crête <olivier.crete@collabora.co.uk>
2010-01-03 20:16:12 -0500

    rtcpfilter: Reduce the packet size when reducing the packet

--- gst/rtcpfilter/fs-rtcp-filter.c
+++ gst/rtcpfilter/fs-rtcp-filter.c
@@ -226,6 +226,8 @@ fs_rtcp_filter_transform_ip (GstBaseTransform *transform, GstBuffer *buf)
             memmove (GST_BUFFER_DATA (buf) + packet.offset + 8,
                 GST_BUFFER_DATA (buf) + nextpacket.offset,
                 GST_BUFFER_SIZE (buf) - nextpacket.offset);
+            GST_BUFFER_SIZE (buf) -= nextpacket.offset - packet.offset - 8;
+
             if (!gst_rtcp_buffer_get_first_packet (buf, &packet))
               break;
           }
