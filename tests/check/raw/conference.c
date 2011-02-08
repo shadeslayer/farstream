9f8848ea4ed24b7aa66d509be5507b02dfa28dd8
Olivier Crête <olivier.crete@collabora.co.uk>
2011-02-08 18:16:22 +0000

    tests: Use set_transmitter for raw tests

--- tests/check/raw/conference.c
+++ tests/check/raw/conference.c
@@ -184,11 +184,16 @@ simple_conference_add_stream (
   fail_if (st->participant == NULL, "Could not make participant, but no GError!");
 
   st->stream = fs_session_new_stream (dat->session, st->participant,
-      FS_DIRECTION_BOTH, transmitter, st_param_count, st_params, &error);
+      FS_DIRECTION_BOTH, NULL, 0, NULL, &error);
   if (error)
     fail ("Error while creating new stream (%d): %s",
         error->code, error->message);
   fail_if (st->stream == NULL, "Could not make stream, but no GError!");
+
+  fail_unless (fs_stream_set_transmitter (st->stream, transmitter, st_params,
+          st_param_count, &error));
+  fail_unless (error == NULL);
+
 
   g_object_set_data (G_OBJECT (st->stream), "SimpleTestStream", st);
 
@@ -1159,11 +1164,14 @@ GST_START_TEST (test_rawconference_errors)
       NULL);
   ts_fail_if (participant == NULL, "Could not create participant");
 
-  stream = fs_session_new_stream (dat->session, participant, FS_DIRECTION_NONE,
-      "invalid-transmitter-name", 0, NULL, &error);
 
-  ts_fail_unless (stream == NULL, "A stream was created with an invalid"
-      " transmitter name");
+
+  stream = fs_session_new_stream (dat->session, participant, FS_DIRECTION_NONE,
+      NULL, 0, NULL, &error);
+  ts_fail_unless (stream != NULL);
+
+  fail_unless (fs_stream_set_transmitter (stream, "invalid-transmitter-name",
+          NULL, 0, &error) == FALSE);
   ts_fail_if (error == NULL, "Error was not set");
   ts_fail_unless (error->domain == FS_ERROR &&
       error->code == FS_ERROR_CONSTRUCTION,
@@ -1300,7 +1308,7 @@ GST_START_TEST (test_rawconference_dispose)
   part = fs_conference_new_participant (conf, &error);
   fail_if (part == NULL || error != NULL);
 
-  stream = fs_session_new_stream (session, part, FS_DIRECTION_BOTH, "rawudp",
+  stream = fs_session_new_stream (session, part, FS_DIRECTION_BOTH, NULL,
       0, NULL, &error);
   fail_if (stream == NULL || error != NULL);
 
@@ -1320,7 +1328,7 @@ GST_START_TEST (test_rawconference_dispose)
 
   g_object_unref (stream);
 
-  stream = fs_session_new_stream (session, part, FS_DIRECTION_BOTH, "rawudp",
+  stream = fs_session_new_stream (session, part, FS_DIRECTION_BOTH, NULL,
       0, NULL, &error);
   fail_if (stream == NULL || error != NULL);
 
